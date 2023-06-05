6bbc4c2ba0d4c75a4452915bc8418aa742f22e19
Gauri Ragade <gauriragade98@gmail.com>
2023-06-05 17:47:41 +0530

    Week array making

---
+++ villagePractice/src/com/power/villagePractice/Array.java
@@ -0,0 +1,23 @@
+package com.power.villagePractice;
+
+public class Array {
+	public static void main(String[] args) {
+		String weekDay[] = new String[7];
+	weekDay	[0]="Monday";
+	weekDay[1]="Thusday";
+	weekDay[2]="wednesday";
+	weekDay[3]="Thursday";
+	weekDay[4]="friday";
+	weekDay[5]="Saterday";
+	weekDay[6]="Sunday";
+	System.out.println("weekday:-");
+	System.out.println(""+weekDay[0]);
+	System.out.println(""+weekDay[1]);
+	System.out.println(""+weekDay[02]);
+	System.out.println(""+weekDay[03]);
+	System.out.println(""+weekDay[04]);
+	System.out.println(""+weekDay[05]);
+	System.out.println(""+weekDay[06]);
+	}
+	
+}
